9a0c43dbfccad0aa95e2419c64bb1331e81eecdb
Oleksii_Stepchuk <stepchyk@gmail.com>
2020-08-20 18:50:13 +0300

    Scripts for create and fill database

---
+++ ext-systems/city-register/src/main/resources/fillTables.sql
@@ -0,0 +1,21 @@
+INSERT INTO cr_district (district_code, district_name)
+VALUES (1, 'Viborskiy');
+
+INSERT INTO cr_street (street_code, street_name)
+VALUES (1, 'Sampsonevskiy prospekt');
+
+INSERT INTO cr_address (district_code, street_code, building, extension, apartment)
+VALUES (1, 1, '10', '2', '121');
+
+INSERT INTO cr_person (sur_name, given_name, patronymic, date_of_birth,
+                       passport_series, passport_number, passport_date, certificate_number, certificate_date)
+VALUES ('Vasilev', 'Pavel', 'Nikolaevich', '1995-03-18', '1234', '123456', '2015-04-11', null, null),
+       ('Vasileva', 'Irina', 'Petrovna', '1997-08-21', '4321', '654321', '2017-09-19', null, null),
+       ('Vasileva', 'Evgenia', 'Pavlovna', '2016-01-11', null, null, null, '456123', '2016-01-21'),
+       ('Vasilev', 'Alexander', 'Pavlovich', '2018-10-24', null, null, null, '321654', '2018-11-09');
+
+INSERT INTO cr_address_person (address_id, person_id, start_date, end_date)
+VALUES (1, 1, '2014-10-12', null),
+       (1, 1, '2014-10-12', null),
+       (1, 1, '2016-02-05', null),
+       (1, 1, '2018-12-11', null);
