4b644b199c0c19e46b911fed9d0f0c679bfbe83e
Oleksii_Stepchuk <stepchyk@gmail.com>
2020-08-21 22:20:42 +0300

    Client realized

---
+++ ext-systems/network-tcp/client/src/main/java/stalex/net/Client.java
@@ -0,0 +1,53 @@
+package stalex.net;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 8; i++) {
+            SimpleClient simpleClient = new SimpleClient(i);
+            simpleClient.start();
+        }
+    }
+}
+
+class SimpleClient extends Thread {
+
+    private final static String[] COMMAND = {
+            "HELLO", "MORNING", "DAY", "EVENING"
+    };
+
+    private final int cmdNumber;
+
+    public SimpleClient(int cmdNumber) {
+        this.cmdNumber = cmdNumber;
+    }
+
+    @Override
+    public void run() {
+        try {
+//            System.out.println("Started: " + LocalDateTime.now());
+            Socket socket = new Socket("127.0.0.1", 25225);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            String command = COMMAND[cmdNumber % COMMAND.length];
+            String sb = command + " Alex";
+            bw.write(sb);
+            bw.newLine();
+            bw.flush();
+
+            String answer = br.readLine();
+            System.out.println("Client got string: " + answer);
+
+            br.close();
+            bw.close();
+//            System.out.println("Finished: " + LocalDateTime.now());
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+}
