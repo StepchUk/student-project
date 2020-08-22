10d0bbf3d899f6ee9c9919194fd6bf6f317b3a31
Oleksii_Stepchuk <stepchyk@gmail.com>
2020-08-22 14:01:42 +0300

    Simple check person in db

--- ext-systems/city-register/src/main/java/loc/stalex/city/web/CheckPersonServlet.java
+++ ext-systems/city-register/src/main/java/loc/stalex/city/web/CheckPersonServlet.java
@@ -1,17 +1,47 @@
 package loc.stalex.city.web;
 
+import loc.stalex.city.dao.PersonCheckDao;
+import loc.stalex.city.domain.PersonRequest;
+import loc.stalex.city.domain.PersonResponse;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
 
 @WebServlet(name = "CheckPersonServlet", urlPatterns = {"/checkPerson"})
 public class CheckPersonServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Get CheckPerson - called");
+        req.setCharacterEncoding("UTF-8");
+
+        String surName = req.getParameter("surName");
+
+        PersonRequest pr = new PersonRequest();
+        pr.setSurName(surName);
+        pr.setGivenName("Pavel");
+        pr.setPatronymic("Nikolaevich");
+        pr.setDateOfBirth(LocalDate.of(1995, 3, 18));
+        pr.setStreetCode(1);
+        pr.setBuilding("10");
+        pr.setExtension("2");
+        pr.setApartment("121");
+
+        try {
+            PersonCheckDao dao = new PersonCheckDao();
+            PersonResponse ps = dao.checkPerson(pr);
+
+            if (ps.isRegistered()) {
+                resp.getWriter().println("Registered");
+            } else {
+                resp.getWriter().println("Not registered");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
