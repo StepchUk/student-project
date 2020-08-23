49a965a64b917868bf256a37a6548b03597e59e0
Oleksii_Stepchuk <stepchyk@gmail.com>
2020-08-23 19:36:13 +0300

    Added json to respons and request

--- ext-systems/city-register/src/main/java/loc/stalex/city/web/CheckPersonService.java
+++ ext-systems/city-register/src/main/java/loc/stalex/city/web/CheckPersonService.java
@@ -1,18 +1,23 @@
 package loc.stalex.city.web;
 
+import loc.stalex.city.domain.PersonRequest;
 import loc.stalex.city.domain.PersonResponse;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/check")
 public class CheckPersonService {
 
-    @GET
-    @Path("/{id}")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public PersonResponse checkPerson(@PathParam("id") int simpleId,
-                                      @QueryParam("name") String simpleName) {
+    public PersonResponse checkPerson(PersonRequest request) {
+        System.out.println(request);
+
         return new PersonResponse();
     }
 }
