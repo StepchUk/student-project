eedff88824d599f8edb5cdc3e9621f768a15b70d
Oleksii_Stepchuk <stepchyk@gmail.com>
2020-08-26 16:30:57 +0300

    Rest client: created simple client with simple response

--- student-order/src/main/java/loc/stalex/studentorder/validator/register/RealCityRegisterChecker.java
+++ student-order/src/main/java/loc/stalex/studentorder/validator/register/RealCityRegisterChecker.java
@@ -1,14 +1,29 @@
 package loc.stalex.studentorder.validator.register;
 
+import loc.stalex.studentorder.domain.register.CityRegisterRequest;
 import loc.stalex.studentorder.domain.register.CityRegisterResponse;
 import loc.stalex.studentorder.domain.Person;
 import loc.stalex.studentorder.exception.CityRegisterException;
 import loc.stalex.studentorder.exception.TransportException;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+
 public class RealCityRegisterChecker implements CityRegisterChecker {
 
     public CityRegisterResponse checkerPerson(Person person)
             throws CityRegisterException, TransportException {
-        return null;
+
+        CityRegisterRequest request = new CityRegisterRequest(person);
+
+        Client client = ClientBuilder.newClient();
+        CityRegisterResponse response = client.target("http://localhost:8080/city-register-1.0/rest/check/")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(request, MediaType.APPLICATION_JSON))
+                .readEntity(CityRegisterResponse.class);
+
+        return response;
     }
 }
